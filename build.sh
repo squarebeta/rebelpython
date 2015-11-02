eb4b4cb13fdc327133543c101a0a4e1d8f1c923f
Emory Merryman <emory.merryman@gmail.com>
2015-11-01 22:19:06 -0500

    add release

--- build.sh
+++ build.sh
@@ -2,13 +2,17 @@
 
 PROJECT=${1} &&
     REPOSITORY=${2} &&
-    MAJOR=${3} &&
-    MINOR=${2} &&
+    RMAJOR=${3} &&
+    RMINOR=${4} &&
+    RPATCH=$(git describe --tags --long | grep "^v${RMAJOR}.${RMINOR}-[0-9]*-.*\$" | sed -e "s#^v${RMAJOR}.${RMINOR}-\([0-9]*\)-.*\$#\1#") &&
+    RELEASE=${RMAJOR}.${RMINOR}.${RPATCH} &&
+    VMAJOR=${5} &&
+    VMINOR=${6} &&
     mkdir --parents build/src/${PROJECT} &&
     cd build/src/${PROJECT} &&
     git clone git@github.com:${PROJECT}/${REPOSITORY}.git &&
-    PATCH=$(git describe --tags --long | grep "^v${MAJOR}.${MINOR}-[0-9]*-.*\$" | sed -e "s#^v${MAJOR}.${MINOR}-\([0-9]*\)-.*\$#\1#") &&
-    VERSION=${MAJOR}.${MINOR}.${PATCH} &&
+    VPATCH=$(git describe --tags --long | grep "^v${VMAJOR}.${VMINOR}-[0-9]*-.*\$" | sed -e "s#^v${VMAJOR}.${VMINOR}-\([0-9]*\)-.*\$#\1#") &&
+    VERSION=${VMAJOR}.${VMINOR}.${VPATCH} &&
     cd ../../.. &&
     mkdir --parents build/spec/${PROJECT} &&
     sed -e "s#\${VERSION}#${VERSION}#" -e "wspec/${REPOSITORY}-${VERSION}.spec" spec/${PROJECT}/${REPOSITORY}.spec &&
